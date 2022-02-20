d2e6a5b12c0c90004a9ce21bb92d627b7a85d5a9
AlukWert67 <82955287+AlukWert67@users.noreply.github.com>
2022-02-20 10:21:38 +0500

    1
    1

--- app/src/main/java/com/example/usainbolty/TipsFrag.java
+++ app/src/main/java/com/example/usainbolty/TipsFrag.java
@@ -1,18 +1,93 @@
 package com.example.usainbolty;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import java.util.ArrayList;
 
 public class TipsFrag extends Fragment {
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.tips_frag, container, false);
+    public static int id1;
 
-        return view;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        View root = inflater.inflate(R.layout.tips_frag, container, false);
+
+        ListView listView = root.findViewById(R.id.listView);
+
+// определяем строковый массив
+        final String[] Nam = new String[] {
+                "E100", "E101", "E102", "E103", "E104",
+                "E105",  "E106",  "E107", "E108", "E109", "E110", "E111", "E120",
+                "E121", "E122", "E123", "E124", "E125", "E126", "E127", "E128", "E129"
+        };
+        ArrayList<String> Names = new ArrayList<String>();
+
+        int[] nazv={R.string.n1,R.string.n2,R.string.n3,R.string.n4,R.string.n5,R.string.n6,R.string.n7};
+
+        for(int i=0;i<Nam.length;i++){
+
+            Names.add(getResources().getString(nazv[i]));
+        }
+
+
+
+
+// используем адаптер данных
+        MyAdapter adapter = new MyAdapter( getContext(),Names);
+
+        listView.setAdapter(adapter);
+
+
+        return root;
+    }
+}
+class MyAdapter extends BaseAdapter {
+    ArrayList<String> data1 = new ArrayList<String>();
+    Context context;
+    MyAdapter(Context context, ArrayList<String> dt1){
+        data1=dt1;
+
+        this.context=context;
+    }
+
+    @Override
+    public int getCount() {
+        return data1.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        if (convertView == null) {
+            convertView = inflater.inflate(R.layout.item, parent, false);
+        }
+
+        TextView txt1 = (TextView) convertView.findViewById(R.id.textView);
+
+        txt1.setText(data1.get(position));
+
+        return convertView;
     }
 }
