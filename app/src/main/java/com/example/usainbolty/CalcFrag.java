3741ff102690ab56b5e0c06bd2436f3844bec41c
YYuuhhh <81140056+YYuuhhh@users.noreply.github.com>
2022-02-20 12:29:44 +0500

    dd

--- app/src/main/java/com/example/usainbolty/CalcFrag.java
+++ app/src/main/java/com/example/usainbolty/CalcFrag.java
@@ -2,17 +2,39 @@ package com.example.usainbolty;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 public class CalcFrag extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        ActionBar actionBar = ((AppCompatActivity)getActivity()).getSupportActionBar();
+
+        // Customize the back button
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_baseline_arrow_back_24);
+
+        // showing the back button in action bar
+        actionBar.setDisplayHomeAsUpEnabled(false);
+
         View view = inflater.inflate(R.layout.calc_frag, container, false);
 
         return view;
+    }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
