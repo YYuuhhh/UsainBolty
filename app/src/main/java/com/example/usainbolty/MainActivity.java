0bf748facba70fe4457ddcd68fdf8bad0bfc8f5a
AlukWert67 <82955287+AlukWert67@users.noreply.github.com>
2022-04-15 16:05:54 +0500

    naziki
    naziki

--- app/src/main/java/com/example/usainbolty/MainActivity.java
+++ app/src/main/java/com/example/usainbolty/MainActivity.java
@@ -1,15 +1,51 @@
-package com.example.usainbolty;
+ package com.example.usainbolty;
 
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
 import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    int b=0;// 0-светлая 1-темная
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu1, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Log.d("tag","kaif");
+        if(item.getItemId() == R.id.theme) {
+            if(b==0){
+
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                    b=1;
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+
+            }
+            else{
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                b=0;
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+            }
+
+
+        }
+        return true;
+    }
+
+    // Остальной код ниже не изменился
+
 
     private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
         public boolean onNavigationItemSelected(MenuItem param1MenuItem) {
@@ -36,13 +72,21 @@ public class MainActivity extends AppCompatActivity {
 
     protected void onCreate(Bundle paramBundle) {
         super.onCreate(paramBundle);
+
         setContentView(R.layout.main_activity);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
         ((BottomNavigationView)findViewById(R.id.nav_view)).setOnNavigationItemSelectedListener(this.navListener);
         if (paramBundle == null)
             getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment, new CalcFrag()).commit();
 
+
     }
+
 }
 
 
