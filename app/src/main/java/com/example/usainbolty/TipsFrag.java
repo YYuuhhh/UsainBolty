f189e8e41e5c6ffcf85315d4890fc547499880ce
YYuuhhh <81140056+YYuuhhh@users.noreply.github.com>
2022-04-16 16:43:50 +0500

    Revert "Merge branch 'nasvay'"
    This reverts commit 853629e2f93e2aacb4c6292f59ffcf9869e34c14, reversing
    changes made to 69d44e4c7e22e697545a0d26b1f19c12b353c166.

--- app/src/main/java/com/example/usainbolty/TipsFrag.java
+++ app/src/main/java/com/example/usainbolty/TipsFrag.java
@@ -1,111 +1,89 @@
 package com.example.usainbolty;
 
-import android.animation.LayoutTransition;
 import android.content.Context;
 import android.content.Intent;
-import android.opengl.Visibility;
 import android.os.Bundle;
-import android.transition.AutoTransition;
-import android.transition.TransitionManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
-import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
-import java.io.File;
 import java.util.ArrayList;
 
 public class TipsFrag extends Fragment {
+    public static int id1;
 
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.tips_frag, container, false);
-        ListView listView = root.findViewById(R.id.listView);
-        FloatingActionButton fab = root.findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar snackbar;
-                snackbar = Snackbar.make(view, "Нажмите на номер совета, чтобы свернуть информацию", Snackbar.LENGTH_SHORT);
-                View snackBarView = snackbar.getView();
-                snackBarView.setBackgroundColor(getResources().getColor(R.color.lightgr));
-                snackbar.show();
-            }
-        });
-        ArrayList<String> main_text = new ArrayList<String>();
-        int[] nazv={R.string.n1,R.string.n2,R.string.n3,R.string.n4,R.string.n5,R.string.n6,R.string.n7};
-        for(int i=0;i<nazv.length;i++){
-            main_text.add(getResources().getString(nazv[i]));
 
+        View root = inflater.inflate(R.layout.tips_frag, container, false);
+
+        ListView listView = root.findViewById(R.id.listView);
+
+
+
+        ArrayList<String> Names = new ArrayList<String>();
+
+        int[] nazv={R.string.n1,R.string.n2,R.string.n3,R.string.n4,R.string.n5,R.string.n6,R.string.n7};
+
+        for(int i=0;i<nazv.length;i++){
+
+            Names.add(getResources().getString(nazv[i]));
         }
-        MyAdapter adapter = new MyAdapter( getContext(), main_text);
+
+
+
+
+// используем адаптер данных
+        MyAdapter adapter = new MyAdapter( getContext(),Names);
+
         listView.setAdapter(adapter);
+
 
         return root;
     }
+}
+class MyAdapter extends BaseAdapter {
+    ArrayList<String> data1 = new ArrayList<String>();
+    Context context;
+    MyAdapter(Context context, ArrayList<String> dt1){
+        data1=dt1;
 
-    private class MyAdapter extends BaseAdapter {
-        int[]images={R.drawable.k1,R.drawable.k2,R.drawable.k3,R.drawable.k4,R.drawable.k5,R.drawable.k6,R.drawable.k7};
-        ArrayList<String> data1 = new ArrayList<String>();
+        this.context=context;
+    }
 
-        Context context;
-        MyAdapter(Context context, ArrayList<String> dt1){
-            data1=dt1;
+    @Override
+    public int getCount() {
+        return data1.size();
+    }
 
-            this.context=context;
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        if (convertView == null) {
+            convertView = inflater.inflate(R.layout.item, parent, false);
         }
-        @Override
-        public int getCount() {
-            return data1.size();
-        }
-        @Override
-        public Object getItem(int position) {
-            return null;
-        }
-        @Override
-        public long getItemId(int position) {
-            return position;
-        }
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            LayoutInflater inflater = LayoutInflater.from(context);
-            if (convertView == null) {
-                convertView = inflater.inflate(R.layout.item, parent, false);
-            }
-            TextView txt1 = (TextView) convertView.findViewById(R.id.main_text);
-            TextView txt2 =(TextView) convertView.findViewById(R.id.heading);
-            ImageView img =(ImageView)convertView.findViewById(R.id.ig);
-           int ps=position+1;
 
-            LinearLayout ln =(LinearLayout) convertView.findViewById(R.id.linear);
+        TextView txt1 = (TextView) convertView.findViewById(R.id.textView);
 
-            CardView card =(CardView) convertView.findViewById(R.id.card_view1);
-            card.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                 int v=(txt1.getVisibility()==View.GONE)? view.VISIBLE: View.GONE;
-                    TransitionManager.beginDelayedTransition(ln,new AutoTransition());
-                    txt1.setVisibility(v);
+        txt1.setText(data1.get(position));
 
-                }
-            });
-            txt2.setText("Совет №"+ps);
-            txt1.setText(data1.get(position));
-            img.setImageResource(images[position]);
-            return convertView;
-        }
+        return convertView;
     }
 }
